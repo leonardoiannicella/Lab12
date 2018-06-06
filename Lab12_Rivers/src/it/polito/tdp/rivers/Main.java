c6126d13ba24b45c2384a715e252050d48e0ddfe
S227257 <S227257@LEPPC14.cdsdom.polito.it>
2018-06-06 16:09:26 +0200

    Sol

--- Lab12_Rivers/src/it/polito/tdp/rivers/Main.java
+++ Lab12_Rivers/src/it/polito/tdp/rivers/Main.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.rivers;
 	
+import it.polito.tdp.rivers.model.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,9 +12,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Rivers.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Rivers.fxml"));
+			BorderPane root = (BorderPane)loader.load();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			RiversController controller = loader.getController();
+			Model model = new Model();
+			controller.setModel(model);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- Lab12_Rivers/src/it/polito/tdp/rivers/db/RiversDAO.java
+++ Lab12_Rivers/src/it/polito/tdp/rivers/db/RiversDAO.java
@@ -3,16 +3,20 @@ package it.polito.tdp.rivers.db;
 import java.util.LinkedList;
 import java.util.List;
 
+import it.polito.tdp.rivers.model.FiumeResult;
+import it.polito.tdp.rivers.model.Flow;
 import it.polito.tdp.rivers.model.River;
+import it.polito.tdp.rivers.model.RiverIdMap;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class RiversDAO {
 
-	public List<River> getAllRivers() {
+	public List<River> getAllRivers(RiverIdMap riverMap) {
 		
 		final String sql = "SELECT id, name FROM river";
 
@@ -24,7 +28,7 @@ public class RiversDAO {
 			ResultSet res = st.executeQuery();
 
 			while (res.next()) {
-				rivers.add(new River(res.getInt("id"), res.getString("name")));
+				rivers.add(riverMap.get(new River(res.getInt("id"), res.getString("name"))));
 			}
 
 			conn.close();
@@ -36,4 +40,64 @@ public class RiversDAO {
 
 		return rivers;
 	}
+
+	public FiumeResult getDatiFiume(River river) {
+		final String sql = "SELECT MIN(day) as dataPrima, MAX(day) as dataUltima, COUNT(*) as count, AVG(flow) as media " + 
+				"FROM flow " + 
+				"WHERE flow.river = ?";
+		FiumeResult fr = null;
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, river.getId());
+			ResultSet res = st.executeQuery();
+
+			if (res.next()) {
+				LocalDate dataPrima = res.getDate("dataPrima").toLocalDate();
+				LocalDate dataUltima = res.getDate("dataUltima").toLocalDate();
+				int misurazioni = res.getInt("count");
+				float media = res.getFloat("media");
+				fr = new FiumeResult(dataPrima, dataUltima, misurazioni, media);
+			}
+			conn.close();
+			if(fr==null)
+				throw new IllegalStateException("Fiume non presente");
+			return fr;
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			throw new RuntimeException("SQL Error");
+		
+		}
+	}
+
+	public List<Flow> getFlowsFromRiver(River river, RiverIdMap RiverMap) {
+		
+		final String sql = "SELECT day, flow.flow as f, flow.river as r " + 
+				"FROM flow " + 
+				"WHERE flow.river = ?";
+
+		List<Flow> flows = new LinkedList<Flow>();
+	
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, river.getId());
+			ResultSet res = st.executeQuery();
+	
+			while (res.next()) {
+				River r = RiverMap.get(res.getInt("r"));
+				flows.add(new Flow(res.getDate("day").toLocalDate(), res.getDouble("f"), r));
+			}
+	
+			conn.close();
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			throw new RuntimeException("SQL Error");
+		}
+	
+		return flows;
+		}
 }
